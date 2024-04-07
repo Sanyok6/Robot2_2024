f70041c0cbe438ace2ccd017a89ffb374552dfcd
Sanyok6 <tishchenkoa2008@gmail.com>
2024-04-07 17:33:54 -0400

    Maintain heading using PID controller in TELEOP.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/TELEOP.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/TELEOP.java
@@ -1,21 +1,37 @@
 package org.firstinspires.ftc.teamcode.opmode;
 
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
 import com.acmerobotics.roadrunner.Pose2d;
 import com.acmerobotics.roadrunner.PoseVelocity2d;
 import com.acmerobotics.roadrunner.Vector2d;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
+import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.teamcode.MecanumDrive;
 import org.firstinspires.ftc.teamcode.subsystems.Arm;
 import org.firstinspires.ftc.teamcode.subsystems.ArmMode;
 
+@Config
 @TeleOp
 public class TELEOP extends LinearOpMode {
+    public static double Kp = 1;
+    public static double Ki = 0;
+    public static double Kd = 0.15;
+    double integralSum = 0;
+    double lastError = 0;
+
+    public static double s = 8;
+
     @Override
     public void runOpMode() {
+        Telemetry telemetry = new MultipleTelemetry(this.telemetry, FtcDashboard.getInstance().getTelemetry());
+
         MecanumDrive drive = new MecanumDrive(hardwareMap, new Pose2d(0,0,0));
 
         Arm arm = new Arm(hardwareMap);
@@ -23,28 +39,59 @@ public class TELEOP extends LinearOpMode {
         Servo planeServo = hardwareMap.servo.get("plane");
         planeServo.setPosition(0.97);
 
+        double targetHeading = drive.imu.get().getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS);
+
         waitForStart();
+
+        ElapsedTime timer = new ElapsedTime();
 
         while (opModeIsActive()) {
 
+            double currentHeading = drive.imu.get().getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS);
+
             double x = -gamepad1.left_stick_y;
             double y = -gamepad1.left_stick_x;
-            double h = drive.imu.get().getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS);
 
             double multiplier = gamepad1.left_trigger > 0.9 ? 0.3 : (gamepad1.right_trigger > 0.9 ? 1 : 0.55);
 
-            double rotatedX = x * Math.cos(-h) - y * Math.sin(-h);
-            double rotatedY = x * Math.sin(-h) + y * Math.cos(-h);
+            double rotatedX = x * Math.cos(-currentHeading) - y * Math.sin(-currentHeading);
+            double rotatedY = x * Math.sin(-currentHeading) + y * Math.cos(-currentHeading);
+
+            targetHeading += -gamepad1.right_stick_x / s;
+
+            if (targetHeading > Math.PI) {
+                targetHeading -= Math.PI * 2;
+            } else if (targetHeading < -Math.PI) {
+                targetHeading += Math.PI * 2;
+            }
+
+            double error;
+            if (Math.abs(targetHeading + 2*Math.PI - currentHeading) < Math.PI) {
+                error = targetHeading + 2*Math.PI - currentHeading;
+            } else if (Math.abs( targetHeading - 2*Math.PI - currentHeading) < Math.PI) {
+                error = targetHeading - 2*Math.PI - currentHeading;
+            } else {
+                error = targetHeading - currentHeading;
+            }
+
+            double derivative = (error - lastError) / timer.seconds();
+            integralSum = integralSum + (error * timer.seconds());
+
+            lastError = error;
+            timer.reset();
+
+            double turnPower = (Kp * error) + (Ki * integralSum) + (Kd * derivative);
 
             drive.setDrivePowers(
                     new PoseVelocity2d(
                             new Vector2d(rotatedX * multiplier, rotatedY * multiplier),
-                            -gamepad1.right_stick_x / 2
+                            turnPower
                     )
             );
 
             if (gamepad1.y) {
                 drive.imu.get().resetYaw();
+                targetHeading = 0;
             }
 
             if (gamepad2.right_bumper && gamepad2.a && arm.mode == ArmMode.DRIVE) {
